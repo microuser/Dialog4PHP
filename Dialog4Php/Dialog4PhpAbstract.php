0a112e27f8007539d8adc42c72293ae41cb4f2cc
capnjeb <capnjeb@inbox.com>
2016-01-25 00:47:25 -0500

    we need to work towards a better class model

---
+++ Dialog4Php/Dialog4PhpAbstract.php
@@ -0,0 +1,16 @@
+<?php
+namespace Dialog4Php;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Dialog4PhpAbstract
+ *
+ * @author user
+ */
+abstract class Dialog4PhpAbstract {
+    public function run(){}    
+}
