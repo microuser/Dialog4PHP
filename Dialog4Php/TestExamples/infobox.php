aab88168cade13543e103e1ff795335b0bd7afae
capnjeb <capnjeb@inbox.com>
2016-01-25 00:47:09 -0500

    we need to work towards a better class model

--- Dialog4Php/TestExamples/infobox.php
+++ Dialog4Php/TestExamples/infobox.php
@@ -1,8 +1,15 @@
 <?php
-
+use Dialog4Php\Dialog4Php;
 include_once(__DIR__.'/../Dialog4Php.php');
 
-$dp = new Dialog4Php();
-$dp->infoBox("This is a test of just body");
-$dp->infoBox("Body", "Title");
-$dp->infoBox("Body","Title","Backtitle");
+(new \Dialog4Php\D4P_InfoBox())
+->setBody("hello")
+->setTitle("someTitle")
+->setBackTitle("backtitle")
+->setColorTheme(1)
+->run();
+
+
+//$dp->infoBox("This is a test of just body");
+//$dp->infoBox("Body", "Title");
+//$dp->infoBox("Body","Title","Backtitle");
